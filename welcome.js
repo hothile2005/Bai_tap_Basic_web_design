58ce7ee7f6940d846eff1ff4e594d108b9c8a2d7
Ho Thi Le <lehothi50@gmail.com>
2026-09-09 23:28:13 +0700

    Done bai 1

---
+++ welcome.js
@@ -0,0 +1,8 @@
+
+    // alert("Welcome to my website!");
+    console.log("Welcome to my website!");
+let sum = 0;
+let a = 10;
+let b = 20;
+sum = a + b;
+console.log("The sum of a and b is: " + sum);
